e0a79cef0a45adac4fe5fe6295eebdcd20f98b53
sultaaaane <mohamadalaoui77@gmail.com>
2024-10-07 22:03:13 +0100

    cppiyat

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: mbentahi <mbentahi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 17:25:43 by mbentahi          #+#    #+#             */
-/*   Updated: 2024/10/07 21:25:11 by mbentahi         ###   ########.fr       */
+/*   Updated: 2024/10/07 21:30:03 by mbentahi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,18 @@ Fixed::~Fixed()
 	std::cout << "Default deconstructor has been called" << std::endl;
 }
 
+Fixed::Fixed(int const value)
+{
+	std::cout << "Int constructor has been called" << std::endl;
+	this->value = value << bits;
+}
+
+Fixed::Fixed(const float value)
+{
+	std::cout << "Float constructor has been called" << std::endl;
+	this->value = roundf(value * (1 << bits));
+}
+
 Fixed &Fixed::operator=(const Fixed &f)
 {
 	if (this != &f)
@@ -38,6 +50,12 @@ Fixed &Fixed::operator=(const Fixed &f)
 		return *this;
 	}
 	return *this;
+}
+
+std::ostream &operator<<(std::ostream &out, Fixed const &f)
+{
+	out << f.toFloat();
+	return out;
 }
 
 int Fixed::getRawBits() const
@@ -51,3 +69,12 @@ void Fixed::setRawBits(int const raw)
 	value = raw;
 }
 
+float Fixed::toFloat() const
+{
+	return ((float)value / (1 << bits));
+}
+
+int Fixed::toInt() const
+{
+	return (value >> bits);
+}
---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbentahi <mbentahi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/03 17:25:46 by mbentahi          #+#    #+#             */
+/*   Updated: 2024/10/07 21:30:37 by mbentahi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <iostream>
+# include <fstream>
+# include <string>
+# include <limits>
+# include <cctype>
+# include <cstdlib>
+# include <iomanip>
+# include <fcntl.h>
+# include <cmath>
+
+class Fixed
+{
+	private:
+		int value;
+		static const int bits = 8;
+	public:
+		Fixed();
+		Fixed(const Fixed &f);
+		Fixed(int const value);
+		Fixed(const float value);
+		~Fixed();
+		Fixed &operator=(const Fixed &f);
+		int getRawBits(void) const;
+		void setRawBits(int const raw);	
+		float toFloat(void) const;
+		int toInt(void) const;
+};
+std::ostream &operator<<(std::ostream &out, Fixed const &f);
